592fadc5631ba2bad2e853107c712218e74b659b
srija14 <59167443+srija14@users.noreply.github.com>
2020-02-08 23:30:52 +0530

    Add files via upload

---
+++ Biscuit.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Biscuit {
+   int weight,price;
+   Sweet(int weight,int price)
+   {
+	   this.weight=weight;
+	   this.price=price;
+   }
+   public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
---
+++ Choco.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Choco {
+	int weight ,price;
+	Choco(int weight, int price)
+	{
+		this.weight=weight;
+		this.price=price;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
